bc7bfc0346d2b7276f23c665c4e2fe55f9bca5a7
Rullyarrfii <rully.jr45@gmail.com>
2025-12-01 18:46:54 +0700

    membuat models password_reset

---
+++ models/password_reset.go
@@ -0,0 +1,12 @@
+package models
+
+import "time"
+
+type PasswordReset struct {
+	IDReset   int32      `gorm:"primaryKey;column:id_reset;type:serial4" json:"id_reset"`
+	UserID    int32      `gorm:"column:user_id;not null" json:"user_id"`
+	Token     string     `gorm:"column:token;size:255;not null" json:"token"`
+	ExpiredAt time.Time  `gorm:"column:expired_at;type:timestamptz;not null" json:"expired_at"`
+	UsedAt    *time.Time `gorm:"column:used_at;type:timestamptz" json:"used_at"`
+	CreatedAt time.Time  `gorm:"column:created_at;type:timestamptz;not null" json:"created_at"`
+}
