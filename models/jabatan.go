1b6f5565a7a8337671f0835d449d552803139b73
Rullyarrfii <rully.jr45@gmail.com>
2025-12-01 18:44:39 +0700

    membuat models jabatan

---
+++ models/jabatan.go
@@ -0,0 +1,7 @@
+package models
+
+type Jabatan struct {
+	IDJabatan   int32   `gorm:"primaryKey;column:id_jabatan;type:serial4" json:"id_jabatan"`
+	NamaJabatan string  `gorm:"column:nama_jabatan;size:100;not null" json:"nama_jabatan"`
+	Deskripsi   *string `gorm:"column:deskripsi" json:"deskripsi"`
+}
