3b55c856be78daf9ee6a135a2f88f4349c19f6c1
Rullyarrfii <rully.jr45@gmail.com>
2025-12-01 18:28:47 +0700

    Membuat models absensi

---
+++ models/absensi.go
@@ -0,0 +1,14 @@
+package models
+
+import "time"
+
+type Absensi struct {
+	IDAbsensi   int32      `gorm:"primaryKey;column:id_absensi;type:serial4" json:"id_absensi"`
+	KaryawanID  int32      `gorm:"column:karyawan_id;not null" json:"karyawan_id"`
+	Tanggal     time.Time  `gorm:"column:tanggal;type:date;not null" json:"tanggal"`
+	WaktuMasuk  *time.Time `gorm:"column:waktu_masuk;type:timestamptz" json:"waktu_masuk"`
+	WaktuPulang *time.Time `gorm:"column:waktu_pulang;type:timestamptz" json:"waktu_pulang"`
+	Status      string     `gorm:"column:status;type:absensi_status_type;not null" json:"status"`
+	Keterangan  *string    `gorm:"column:keterangan" json:"keterangan"`
+	Lokasi      *string    `gorm:"column:lokasi;size:255" json:"lokasi"`
+}
